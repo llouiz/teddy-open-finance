7d3feb438f340571fb74d615ead94f2dbe6b858c
llouiz <luiz.carlos.techie@gmail.com>
2025-02-18 23:00:07 -0300

    feat: :rocket: menu de navegação
    refs: #5

---
+++ projects/host-app/src/app/navbar/navbar.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+    userInfo = {
+      username: ''
+    };
+  
+    constructor(
+      private router: Router,
+      private cookieService: CookieService
+    ) {};
+  
+    ngOnInit() {
+      this.userInfo = this.getUserInfo();
+    }
+  
+    getUserInfo() {
+      let userInfo = localStorage.getItem('userInfo');
+  
+      if (!userInfo) {
+          userInfo = this.cookieService.get('userInfo');
+      }
+  
+      return userInfo ? JSON.parse(userInfo) : null;
+    }
+
+    logout() {
+      localStorage.removeItem('userInfo');
+
+      this.cookieService.delete('userInfo');
+      
+      this.router.navigateByUrl('');
+    }
+}
